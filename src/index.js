8b02c2f59481453e6a2bcea653bd145f61ae8701
FRXZZ3N <timurlan.ulanuulu@mail.ru>
2023-01-30 18:18:06 +0600

    added footer

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-   <React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
       <Header />
-      <App />
+      <div style={{ height: "64vh" }}>
+        <App />
+      </div>
       <Footer />
-   </React.StrictMode>
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
