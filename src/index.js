2fd6f7aa49a99f2e3dbad97c363309e5cfdc065a
FRXZZ3N <timurlan.ulanuulu@mail.ru>
2023-01-30 20:11:59 +0600

    added task menu

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom/dist';
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+   <BrowserRouter>
    <React.StrictMode>
       <Header />
       <App />
       <Footer />
    </React.StrictMode>
+   </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
