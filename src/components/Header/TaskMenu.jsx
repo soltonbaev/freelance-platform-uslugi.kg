02558aaaa5fe09c7d27addbe8b9aea43cd52169f
soltonbaev <isoltonbaev@gmail.com>
2023-02-01 10:53:57 +0600

    added getting services and categoriesc from db

--- src/components/Header/TaskMenu.jsx
+++ src/components/Header/TaskMenu.jsx
@@ -1,163 +1,173 @@
-import { Grid, List, ListItem, Typography } from "@mui/material";
-import { Box, Container } from "@mui/system";
-import React from "react";
-import { Link } from "react-router-dom";
-import taskMenu from "./css/taskMenu.css";
+import {Grid, List, ListItem, Typography} from '@mui/material';
+import {Box, Container} from '@mui/system';
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {useGlobalContext} from '../../contexts/GlobalContextProvider';
+import taskMenu from './css/taskMenu.css';
 
 const TaskMenu = () => {
-  const services = [
-    {
-      title: "Moving Services",
-      link: "#",
-      subServices: [
-        { name: "Help Moving", link: "#", id: 1 },
-        { name: "Heavy Lifting", link: "#", id: 2 },
-        { name: "Furniture Movers", link: "#", id: 3 },
-        { name: "Full Service Help Moving", link: "#", id: 4 },
-      ],
-    },
-    {
-      title: "Mounting & Installation",
-      link: "#",
-      subServices: [
-        { name: "TV Mounting", link: "#", id: 1 },
-        { name: "Shelf Mounting", link: "#", id: 2 },
-        { name: "Hang Christmas Lights", link: "#", id: 3 },
-        { name: "Ceiling Fan Installation", link: "#", id: 4 },
-      ],
-    },
-    {
-      title: "Cleaning",
-      link: "#",
-      subServices: [
-        { name: "House Cleaning Services", link: "#", id: 1 },
-        { name: "Deep Cleaning", link: "#", id: 2 },
-        { name: "Car Washing", link: "#", id: 3 },
-        { name: "Laundry Help", link: "#", id: 4 },
-        { name: "Window Cleaning", link: "#", id: 5 },
-      ],
-    },
-    {
-      title: "Furniture Assembly",
-      link: "#",
-      subServices: [
-        { name: "Furniture Assembly", link: "#", id: 1 },
-        { name: "Bed Assembly", link: "#", id: 2 },
-        { name: "Bookshelf Assembly", link: "#", id: 3 },
-        { name: "Disassemble furniture", link: "#", id: 4 },
-      ],
-    },
-    {
-      title: "Yardwork Services",
-      link: "#",
-      subServices: [
-        { name: "Gardening Services", link: "#", id: 1 },
-        { name: "Lawn Care Services", link: "#", id: 2 },
-        { name: "Gutter Cleaning", link: "#", id: 3 },
-        { name: "Hedge Trimming Service", link: "#", id: 4 },
-        { name: "Hose Installation", link: "#", id: 5 },
-        { name: "Fence Staining", link: "#", id: 6 },
-      ],
-    },
-    {
-      title: "Winter Tasks",
-      link: "#",
-      subServices: [
-        { name: "Snow Removal", link: "#", id: 1 },
-        { name: "Sidewalk Salting", link: "#", id: 2 },
-        { name: "Window Winterizationg", link: "#", id: 3 },
-        { name: "Storm Door Installation", link: "#", id: 4 },
-        { name: "Water Heater Maintenance", link: "#", id: 5 },
-      ],
-    },
-  ];
+   const {getServiceCategories, getServices} = useGlobalContext();
+   console.log('categories array', getServiceCategories());
+   console.log('services array', getServices());
+   const services = [
+      {
+         title: 'Moving Services',
+         link: '#',
+         subServices: [
+            {name: 'Help Moving', link: '#', id: 1},
+            {name: 'Heavy Lifting', link: '#', id: 2},
+            {name: 'Furniture Movers', link: '#', id: 3},
+            {name: 'Full Service Help Moving', link: '#', id: 4},
+         ],
+      },
+      {
+         title: 'Mounting & Installation',
+         link: '#',
+         subServices: [
+            {name: 'TV Mounting', link: '#', id: 1},
+            {name: 'Shelf Mounting', link: '#', id: 2},
+            {name: 'Hang Christmas Lights', link: '#', id: 3},
+            {name: 'Ceiling Fan Installation', link: '#', id: 4},
+         ],
+      },
+      {
+         title: 'Cleaning',
+         link: '#',
+         subServices: [
+            {name: 'House Cleaning Services', link: '#', id: 1},
+            {name: 'Deep Cleaning', link: '#', id: 2},
+            {name: 'Car Washing', link: '#', id: 3},
+            {name: 'Laundry Help', link: '#', id: 4},
+            {name: 'Window Cleaning', link: '#', id: 5},
+         ],
+      },
+      {
+         title: 'Furniture Assembly',
+         link: '#',
+         subServices: [
+            {name: 'Furniture Assembly', link: '#', id: 1},
+            {name: 'Bed Assembly', link: '#', id: 2},
+            {name: 'Bookshelf Assembly', link: '#', id: 3},
+            {name: 'Disassemble furniture', link: '#', id: 4},
+         ],
+      },
+      {
+         title: 'Yardwork Services',
+         link: '#',
+         subServices: [
+            {name: 'Gardening Services', link: '#', id: 1},
+            {name: 'Lawn Care Services', link: '#', id: 2},
+            {name: 'Gutter Cleaning', link: '#', id: 3},
+            {name: 'Hedge Trimming Service', link: '#', id: 4},
+            {name: 'Hose Installation', link: '#', id: 5},
+            {name: 'Fence Staining', link: '#', id: 6},
+         ],
+      },
+      {
+         title: 'Winter Tasks',
+         link: '#',
+         subServices: [
+            {name: 'Snow Removal', link: '#', id: 1},
+            {name: 'Sidewalk Salting', link: '#', id: 2},
+            {name: 'Window Winterizationg', link: '#', id: 3},
+            {name: 'Storm Door Installation', link: '#', id: 4},
+            {name: 'Water Heater Maintenance', link: '#', id: 5},
+         ],
+      },
+   ];
 
-  return (
-    <Box sx={{ mt: "10px", mb: "10px" }}>
-      <Container>
-        <Box
-          sx={{
-            minHeight: "30px",
-            // width: "100%",
-            flexGrow: 1,
-            display: { sm: "block", xs: "none" },
-          }}
-        >
-          <Grid
-            container
-            spacing={0}
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            {services.map((category) => (
-              <Grid className="services" sx={{ width: "180px" }}>
-                <Box
-                  className="servicesItem"
-                  sx={{ textAlign: "center", minHeight: "30px" }}
-                >
-                  <Link to="*">{category.title}</Link>
-
-                  <ul className="servicesList">
-                  <li style={{fontWeight: "bold"}}>Популярные услуги</li>
-                    {category.subServices.map((item) => (
-                      <li>
-                        <Link to={item.link}>{item.name}</Link>
-                      </li>
-                    ))}
-                  </ul>
-                </Box>
-              </Grid>
-            ))}
-          </Grid>
-        </Box>
-
-        {/* ==================================================================== */}
-
-        <Box
-          sx={{
-            minHeight: "30px",
-            // width: "100%",
-            flexGrow: 1,
-            display: { sm: "none", xs: "block" },
-          }}
-        >
-          <ul className="servicesMenu">
-            <Typography
-              sx={{ borderLeft: "1px solid black", paddingLeft: "5px" }}
+   return (
+      <Box sx={{mt: '10px', mb: '10px'}}>
+         <Container>
+            <Box
+               sx={{
+                  minHeight: '30px',
+                  // width: "100%",
+                  flexGrow: 1,
+                  display: {sm: 'block', xs: 'none'},
+               }}
             >
-              Services
-            </Typography>
-            <li className="servicesMenuList">
-              {services.map((category) => (
-                <li>
-                  <Grid className="services" sx={{ width: "180px" }}>
-                    <Box
-                      className="servicesItem"
-                      sx={{ textAlign: "center", minHeight: "30px" }}
-                    >
-                      <Typography>{category.title}</Typography>
+               <Grid
+                  container
+                  spacing={0}
+                  sx={{
+                     display: 'flex',
+                     justifyContent: 'space-between',
+                     alignItems: 'center',
+                  }}
+               >
+                  {services.map(category => (
+                     <Grid className="services" sx={{width: '180px'}}>
+                        <Box
+                           className="servicesItem"
+                           sx={{textAlign: 'center', minHeight: '30px'}}
+                        >
+                           <Link to="*">{category.title}</Link>
 
-                      <ul className="servicesList">
-                      <li style={{fontWeight: "bold"}}>Популярные услуги</li>
-                        {category.subServices.map((item) => (
-                          <li>
-                            <Link to={item.link}>{item.name}</Link>
-                          </li>
-                        ))}
-                      </ul>
-                    </Box>
-                  </Grid>
-                </li>
-              ))}
-            </li>
-          </ul>
-        </Box>
-      </Container>
-    </Box>
-  );
+                           <ul className="servicesList">
+                              <li style={{fontWeight: 'bold'}}>
+                                 Популярные услуги
+                              </li>
+                              {category.subServices.map(item => (
+                                 <li>
+                                    <Link to={item.link}>{item.name}</Link>
+                                 </li>
+                              ))}
+                           </ul>
+                        </Box>
+                     </Grid>
+                  ))}
+               </Grid>
+            </Box>
+
+            {/* ==================================================================== */}
+
+            <Box
+               sx={{
+                  minHeight: '30px',
+                  // width: "100%",
+                  flexGrow: 1,
+                  display: {sm: 'none', xs: 'block'},
+               }}
+            >
+               <ul className="servicesMenu">
+                  <Typography
+                     sx={{borderLeft: '1px solid black', paddingLeft: '5px'}}
+                  >
+                     Services
+                  </Typography>
+                  <li className="servicesMenuList">
+                     {services.map(category => (
+                        <li>
+                           <Grid className="services" sx={{width: '180px'}}>
+                              <Box
+                                 className="servicesItem"
+                                 sx={{textAlign: 'center', minHeight: '30px'}}
+                              >
+                                 <Typography>{category.title}</Typography>
+
+                                 <ul className="servicesList">
+                                    <li style={{fontWeight: 'bold'}}>
+                                       Популярные услуги
+                                    </li>
+                                    {category.subServices.map(item => (
+                                       <li>
+                                          <Link to={item.link}>
+                                             {item.name}
+                                          </Link>
+                                       </li>
+                                    ))}
+                                 </ul>
+                              </Box>
+                           </Grid>
+                        </li>
+                     ))}
+                  </li>
+               </ul>
+            </Box>
+         </Container>
+      </Box>
+   );
 };
 
 export default TaskMenu;
--- src/contexts/GlobalContextProvider.js
+++ src/contexts/GlobalContextProvider.js
@@ -1,81 +1,112 @@
-import { doc, getDoc } from "firebase/firestore";
-import React, { createContext, useContext, useEffect, useState } from "react";
-import fireBase, { db } from "../helpers/firebase";
+import {doc, getDoc, getDocs, collection} from 'firebase/firestore';
+import React, {createContext, useContext, useEffect, useState} from 'react';
+import fireBase, {db} from '../helpers/firebase';
 
 export const globalContext = createContext();
 export const useGlobalContext = () => useContext(globalContext);
 
-const GlobalContextProvider = ({ children }) => {
-  const services = [
-    {
-      title: "Moving Services",
-      link: "#",
-      imgUrl:
-        "https://www.bostonmagazine.com/wp-content/uploads/sites/2/2019/06/moving-season-guide.jpg",
-      subServices: [
-        { name: "Help Moving", link: "#", id: 1 },
-        { name: "Heavy Lifting", link: "#", id: 2 },
-        { name: "Furniture Movers", link: "#", id: 3 },
-        { name: "Full Service Help Moving", link: "#", id: 4 },
-      ],
-    },
-  ];
-  let [user, setUser] = useState("");
-  let [hasAccount, setHasAccount] = useState("");
-  let [isLoggedIn, setIsLoggedIn] = useState(false);
-  let [userDetails, setUserDetails] = useState({});
-  let [test, setTest] = useState("Hellooooooo!!");
-  let [isUserWorker, setIsUserWorker] = useState(false);
+const GlobalContextProvider = ({children}) => {
+   const services = [
+      {
+         title: 'Moving Services',
+         link: '#',
+         imgUrl:
+            'https://www.bostonmagazine.com/wp-content/uploads/sites/2/2019/06/moving-season-guide.jpg',
+         subServices: [
+            {name: 'Help Moving', link: '#', id: 1},
+            {name: 'Heavy Lifting', link: '#', id: 2},
+            {name: 'Furniture Movers', link: '#', id: 3},
+            {name: 'Full Service Help Moving', link: '#', id: 4},
+         ],
+      },
+   ];
+   let [user, setUser] = useState('');
+   let [hasAccount, setHasAccount] = useState('');
+   let [isLoggedIn, setIsLoggedIn] = useState(false);
+   let [userDetails, setUserDetails] = useState({});
+   let [test, setTest] = useState('Hellooooooo!!');
+   let [isUserWorker, setIsUserWorker] = useState(false);
 
-  const authListener = () => {
-    fireBase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        setUser(user);
-        async function getFromDb() {
-          const docRef = doc(db, "userData", user.uid);
-          // console.log('uid', user.uid);
-          const docSnap = await getDoc(docRef);
-          if (docSnap.exists()) {
-            let docSnapData = docSnap.data();
-            console.log(docSnapData);
-            setUserDetails(docSnapData);
-          } else {
-            // doc.data() will be undefined in this case
-            console.log("No such document!");
-          }
-        }
-        getFromDb();
-      } else {
-        setUser("");
+   function getServiceCategories() {
+      const arr = [];
+      async function getData() {
+         const querySnapshot = await getDocs(
+            collection(db, 'admin', 'servicesData', 'categories')
+         );
+
+         querySnapshot.forEach(doc => {
+            arr.push(doc.data());
+         });
       }
-    });
-  };
+      getData();
+      return arr;
+   }
 
-  useEffect(() => {
-    authListener();
-    console.log("userDetails", userDetails.email);
-  }, []);
+   function getServices() {
+      const arr = [];
+      async function getData() {
+         const querySnapshot = await getDocs(
+            collection(db, 'admin', 'servicesData', 'services')
+         );
+         querySnapshot.forEach(doc => {
+            arr.push(doc.data());
+         });
+      }
+      getData();
+      return arr;
+   }
 
-  // useEffect(() => {
-  //    console.log(user);
-  // }, [user]);
-  let value = {
-    services,
-    user,
-    setUser,
-    hasAccount,
-    setHasAccount,
-    test,
-    isLoggedIn,
-    setIsLoggedIn,
-    userDetails,
-    setUserDetails,
-    isUserWorker,
-    setIsUserWorker,
-  };
-  return (
-    <globalContext.Provider value={value}>{children}</globalContext.Provider>
-  );
+   const authListener = () => {
+      fireBase.auth().onAuthStateChanged(user => {
+         if (user) {
+            setUser(user);
+            async function getFromDb() {
+               const docRef = doc(db, 'userData', user.uid);
+               // console.log('uid', user.uid);
+               const docSnap = await getDoc(docRef);
+               if (docSnap.exists()) {
+                  let docSnapData = docSnap.data();
+                  console.log(docSnapData);
+                  setUserDetails(docSnapData);
+               } else {
+                  // doc.data() will be undefined in this case
+                  console.log('No such document!');
+               }
+            }
+            getFromDb();
+         } else {
+            setUser('');
+         }
+      });
+   };
+
+   useEffect(() => {
+      authListener();
+      console.log('userDetails', userDetails.email);
+   }, []);
+
+   // useEffect(() => {
+   //    console.log(user);
+   // }, [user]);
+   let value = {
+      services,
+      user,
+      setUser,
+      hasAccount,
+      setHasAccount,
+      test,
+      isLoggedIn,
+      setIsLoggedIn,
+      userDetails,
+      setUserDetails,
+      isUserWorker,
+      setIsUserWorker,
+      getServiceCategories,
+      getServices,
+   };
+   return (
+      <globalContext.Provider value={value}>{children}</globalContext.Provider>
+   );
 };
 
 export default GlobalContextProvider;
