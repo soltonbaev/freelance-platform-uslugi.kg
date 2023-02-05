40618bce5cf13195522a04fcac4ea96de556a2fa
FRXZZ3N <timurlan.ulanuulu@mail.ru>
2023-02-05 17:25:50 +0600

    chatBox

--- src/components/content/private/chat/ChatHeader.jsx
+++ src/components/content/private/chat/ChatHeader.jsx
@@ -1,22 +1,26 @@
-import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material';
-import React from 'react';
-import { useGlobalContext } from '../../../../contexts/GlobalContextProvider';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import { useNavigate } from 'react-router-dom';
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import React from "react";
+import { useGlobalContext } from "../../../../contexts/GlobalContextProvider";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { useNavigate } from "react-router-dom";
+import { Container } from "@mui/system";
 
 const ChatHeader = () => {
-    const {chatWithUser, setIsChatActive} = useGlobalContext();
-    const navigate = useNavigate()
-    console.log("asd",chatWithUser);
-    return (
-        <Box sx={{ flexGrow: 1,  }}>
-        <AppBar position="static" sx={{background: "#4c768d"}}>
+  const { chatWithUser, setIsChatActive } = useGlobalContext();
+  const navigate = useNavigate();
+  console.log("asd", chatWithUser);
+  return (
+    <Container>
+      <Box sx={{ display: "flex", justifyContent: "center"}}>
+        <AppBar position="fixed" sx={{ background: "#4c768d", margin: "auto", width: "100vw", padding: "10px 10px", top: "0", right: "0", left: "0"  }}>
           <Toolbar variant="dense">
-            <Button onClick={() => {
+            <Button
+              onClick={() => {
                 setIsChatActive(false);
-                navigate("/my-tasks")
-            }}>
-                <ArrowBackIcon sx={{color: "white"}}/>
+                navigate("/my-tasks");
+              }}
+            >
+              <ArrowBackIcon sx={{ color: "white" }} />
             </Button>
             <Typography variant="h6" color="inherit" component="div">
               {chatWithUser}
@@ -24,7 +28,8 @@ const ChatHeader = () => {
           </Toolbar>
         </AppBar>
       </Box>
-    );
+    </Container>
+  );
 };
 
 export default ChatHeader;
